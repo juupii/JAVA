2925f3a46e4fd3d5cc9af93a303ced7568367ce2
Roshan kumar <109273319+juupii@users.noreply.github.com>
2022-08-02 23:04:06 +0530

    Add files via upload

---
+++ CodeChef/Bidding.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Bidding{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int a =sc.nextInt();
+            int b =sc.nextInt();
+            int c =sc.nextInt();
+            if(a>b && a>c)
+            {
+                System.out.println("ALICE");
+            }
+            else if(b>a && b>c)
+            {
+                System.out.println("BOB");
+            }
+            else
+            {
+                System.out.println("CHARLIE");
+            }
+        }
+        sc.close();
+    }
+}
