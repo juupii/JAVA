ccd92d8a6cf784f7be684466898f8cd04c77ff3e
Roshan kumar <109273319+juupii@users.noreply.github.com>
2022-08-12 21:36:00 +0530

    Add files via upload

---
+++ CodeChef/TheLeadGame.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class TheLeadGame {
+static void result(int[] arr , int[] arr1)
+{
+    1
+}
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] player1 = new int[n];
+    int[] player2 = new int[n];
+   // while(n-->0)
+    //{
+        for(int i = 1; i <= n+1;i++)
+        {
+            player1[i] = sc.nextInt();
+            player2[i] = sc.nextInt();
+            
+        }
+        
+        
+          
+        }
+        
+    }
+
+   
+
