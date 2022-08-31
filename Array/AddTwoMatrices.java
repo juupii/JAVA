5e0cf013ccf67678b9f67e7fd171d21b9b344f4a
Roshan kumar <109273319+juupii@users.noreply.github.com>
2022-08-31 23:05:56 +0530

    Update AddTwoMatrices.java

--- Array/AddTwoMatrices.java
+++ Array/AddTwoMatrices.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class AddTwoMatrices {
+public class AddTwoMatrices  {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the rows of 1st matrix: ");
