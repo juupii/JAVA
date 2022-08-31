d0052a48155939c0d016228437e69c5916d6de3e
Roshan kumar <109273319+juupii@users.noreply.github.com>
2022-08-31 23:06:20 +0530

    Update ArraySum.java

--- Array/ArraySum.java
+++ Array/ArraySum.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ArraySum {
+public class ArraySum  {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array: ");
